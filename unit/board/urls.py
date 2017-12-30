f43453a0a982b14787fcd11ae319d04b9078cb96
hodongman <arshavin3@naver.com>
2017-12-30 15:05:40 +0900

    url update

--- unit/board/urls.py
+++ unit/board/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from board import views
 
+from .views import BoardList, BoardDetail, CommentList, CommentDetail
 
 urlpatterns = [
-
+    url(r'^$', BoardList.as_view(), name=BoardList.name),
+    url(r'^(?P<pk>[0-9]+)/$', BoardDetail.as_view(), name=BoardDetail.name),
+    url(r'^comment/$', CommentList.as_view(), name=BoardList.name),
+    url(r'^comment/(?P<pk>[0-9]+)/$', CommentDetail.as_view(), name=BoardDetail.name),
 ]
